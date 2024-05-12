d642bc64510ad049e8e5f63ee66d5afbaaa193e2
JaskierBared <mateuszswiderski009@gmail.com>
2024-05-12 10:41:27 +0200

    one component to view informations

--- src/components/CvCard/RightSite.tsx
+++ src/components/CvCard/RightSite.tsx
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CvCard.css";
 import { CategoryButton } from "./common/CategoryButton/CategoryButton";
-import { AboutMe } from "./AboutMe";
-import { Skills } from "./Skills";
-import { Portfolio } from "./Portfolio";
 
 interface Props {
   activeTab: string;
 }
 
-export const RightSite: React.FC<Props> = ({ activeTab }) => {
+export const RightSite = (props: Props) => {
+  const [activeButton, setActiveButton] = useState("");
 
+  const AboutMeData = [
+    { 'więcej o mnie': 'więcej o mnie' },
+    { 'edukacja': 'edukacja' },
+    { 'Przycisk 3': 'Przycisk 3' },
+    { 'Umiejętności': 'umiejętności' }
+  ];
+
+  const PortfolioData = [
+    { 'Gapp': 'Gapp' },
+    { 'Gothinczuyk': 'Gothinczyk' },
+    { 'HeadHunters': 'Headhunters' },
+    { 'PomodoroEq': 'PomodoroEq' }
+  ];
+
+  const SkillsData = [
+    { 'Frontend': 'Frontend' },
+    { 'Backend': 'Backend' },
+    { 'AI': 'AI' },
+    { 'OtherTools': 'OtherTools' }
+  ];
+
+  const getCategoryData = (category: string) => {
+    switch (category) {
+      case 'AboutMe':
+        return AboutMeData;
+      case 'Portfolio':
+        return PortfolioData;
+      case 'Skills':
+        return SkillsData;
+      default:
+        return [];
+    }
+  };
+
+  const handleButtonClick = (label: string) => {
+    setActiveButton(activeButton === label ? "" : label);
+  };
+
+  const categoryData = getCategoryData(props.activeTab);
 
   return (
     <div className="right">
-      {activeTab === "CV" && <button className="button">Pobierz CV</button>}
-      {activeTab === "O mnie" && <AboutMe />}
-      {activeTab === "Portfolio" && <Portfolio/>}
-      {activeTab === "Umiejętności" && <Skills/>}
+      {categoryData.map((item, index) => (
+        <CategoryButton
+          key={index}
+          label={Object.keys(item)[0]}
+          content={Object.values(item)[0]}
+          onClick={() => handleButtonClick(Object.keys(item)[0])}
+          isContentVisible={activeButton === Object.keys(item)[0]}
+          icon=""
+        />
+      ))}
     </div>
   );
 };
